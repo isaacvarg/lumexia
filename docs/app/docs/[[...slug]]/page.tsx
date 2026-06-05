a4ed197756a6c020377a30a833d07e896b405e0d
Isaac Vargas <hello@isaacvargas.dev>
2026-06-04 18:25:01 -0700

    update installations docs

--- docs/app/docs/[[...slug]]/page.tsx
+++ docs/app/docs/[[...slug]]/page.tsx
@@ -4,7 +4,6 @@ import {
   DocsDescription,
   DocsPage,
   DocsTitle,
-  MarkdownCopyButton,
   ViewOptionsPopover,
 } from 'fumadocs-ui/layouts/docs/page';
 import { notFound } from 'next/navigation';
@@ -26,12 +25,14 @@ export default async function Page(props: PageProps<'/docs/[[...slug]]'>) {
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription className="mb-0">{page.data.description}</DocsDescription>
       <div className="flex flex-row gap-2 items-center border-b pb-6">
-        <MarkdownCopyButton markdownUrl={markdownUrl} />
+        {/*  unsure if i want this
         <ViewOptionsPopover
           markdownUrl={markdownUrl}
           githubUrl={`https://github.com/${gitConfig.user}/${gitConfig.repo}/blob/${gitConfig.branch}/content/docs/${page.path}`}
         />
+        */}
       </div>
+
       <DocsBody>
         <MDX
           components={getMDXComponents({
