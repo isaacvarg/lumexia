5617af086a54f7a4559adcb07d0f8bdaf67933ad
Isaac Vargas <hello@isaacvargas.dev>
2026-07-02 11:56:13 -0700

    docs: fix naming of the docs app

--- docs/lib/shared.ts
+++ docs/lib/shared.ts
@@ -1,4 +1,4 @@
-export const appName = 'Lumexia Docs';
+export const appName = 'Lumexia';
 export const docsRoute = '/docs';
 export const docsImageRoute = '/og/docs';
 export const docsContentRoute = '/llms.mdx/docs';
